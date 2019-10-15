1d4e2b2e09c3701090b988fd604d2fe80c39b094
JustaConfusedGuy <56404874+JustaConfusedGuy@users.noreply.github.com>
2019-10-15 14:28:54 +0200

    Added Project

---
+++ Lamp_effekt_för_tända/Assets/Scenes/pushed_object.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pushed_object : MonoBehaviour
+{
+   
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "fly")
+        {
+            rb.AddForce(-250, 250, 0);
+        }
+        }
+
+
+
+
+}
+
+/*
+ * 
+ *rb.AddForce(0, Impulse, 0, ForceMode.Impulse);
+ * 
+*/
