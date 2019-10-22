99d5f6c18f9ac344d533820e3d3e4e913c714d76
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2019-10-22 15:41:58 +0200

    slut

---
+++ Rube G Klart/Assets/Scripts/partcleStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class partcleStart : MonoBehaviour
+{
+    [SerializeField]
+    GameObject particle;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "vägg")
+        {
+            Instantiate(particle, new Vector3(40, 9 , -9), Quaternion.identity);
+        }
+    }
+}
