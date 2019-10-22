06a9448cc9946c398a47b72b3630957397289e4e
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2019-10-22 13:52:40 +0200

    Får ihop alla projekt i ett

---
+++ Rube G Klart/Assets/Scripts/BollTillRaket.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BollTillRaket : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject raketFlöde;
+
+    Transform raketTransform;
+
+    [SerializeField]
+    GameObject raket;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // Definera rb
+        raketTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Boll")
+        {
+            rb.velocity = new Vector3(0,10,0);
+        }
+    }
+
+}
