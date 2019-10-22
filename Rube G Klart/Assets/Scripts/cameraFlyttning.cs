4bf4c36975d91d626f56bee7667703d5ed33f64a
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2019-10-22 14:37:01 +0200

    Kamra script

---
+++ Rube G Klart/Assets/Scripts/cameraFlyttning.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFlyttning : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Raket")
+        {
+            camera.transform.position = new Vector3(6, 19, -5);
+        }
+    }
+
+}
